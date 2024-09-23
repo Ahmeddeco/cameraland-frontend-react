72ba747640a62ca15074a98d4d0c12963d0d3a9e
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-09-23 16:59:00 +0300

    Analytics

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import CartProvider from './context/CartContext'
+import { Analytics } from '@vercel/analytics/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
 		<CartProvider>
+			<Analytics />
 			<App />
 		</CartProvider>
 	</React.StrictMode>
